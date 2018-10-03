1e2caf9583b8ed1859899e1391f81fad0f612053
Unknown <pruinjanek@gmail.com>
2018-10-03 16:17:26 +0200

    Added Winning-Function and GameManager

---
+++ Cowbomb/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+	//Variables
+	public bool gameWon = false;
+	public GameObject winningScreen;
+
+	//Win Game
+	public void Win()
+	{
+		if (!gameWon)
+		{
+			gameWon = true;
+			winningScreen.SetActive (true);
+			Debug.Log ("Won");
+		}
+	}
+}
--- Cowbomb/Assets/Scripts/Scissors.cs
+++ Cowbomb/Assets/Scripts/Scissors.cs
@@ -9,6 +9,7 @@ public class Scissors : MonoBehaviour
 	private float elapsedFrames = 0;
 	public float range;
 	public float wobbleSpeed;
+	public GameManagerScript gameManager;
 
 	// Use this for initialization
 	void Start () 
@@ -22,5 +23,11 @@ public class Scissors : MonoBehaviour
 		elapsedFrames += 0.1f;
 		transform.position = new Vector3 (startPosition.x, startPosition.y + Mathf.Sin (elapsedFrames * wobbleSpeed) * range, startPosition.z);
 		transform.Rotate (new Vector3 (0, 1, 0));
+	}
+
+	//Win Game
+	void OnTriggerEnter()
+	{
+		gameManager.Win ();
 	}
 }
