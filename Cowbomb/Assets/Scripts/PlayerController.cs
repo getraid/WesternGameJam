2876935d3433bf0390f49ee77e04ce7c37e3c354
Unknown <pruinjanek@gmail.com>
2018-10-03 02:20:56 +0200

    Reverse Commit

---
+++ Cowbomb/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	//Component References
+	public Camera playerCamera;
+	Rigidbody myRigidbody;
+
+	//Movement Variables
+	public float moveSpeed;
+	public float gravity;
+	public float jumpforce;
+
+	//Initialization
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody>();
+	}
+
+	//Fixed Update
+	void FixedUpdate() 
+	{
+		Movement();	
+	}
+
+	//Movement
+	void Movement()
+	{
+		//myRigidbody.MovePosition(transform.right * 
+	}
+}
